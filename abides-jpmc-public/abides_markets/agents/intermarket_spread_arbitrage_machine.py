847a4b0c12f2b87bedb5cce4243643b9b6e28d1b
phelstab <paulhelsta@gmail.com>
2022-12-13 11:33:17 +0100

    update

---
+++ abides-jpmc-public/abides_markets/agents/intermarket_spread_arbitrage_machine.py
@@ -0,0 +1,4 @@
+
+# https://www.investopedia.com/terms/i/intermarketspread.asp
+# https://www.mdpi.com/1911-8074/3/1/63
+# https://books.google.de/books?hl=en&lr=&id=PIpxrK3LLUMC&oi=fnd&pg=PA17&dq=Intermarket+Spread+trading+strategy&ots=7REtaF4kZP&sig=B0PUQLtPjQ6SWhp6JXti3RQDX7s&redir_esc=y#v=onepage&q=Intermarket%20Spread%20trading%20strategy&f=false
