90a442b0681a1384964541f411001d66a1f88633
Paul Helstab <paulhelstab@gmail.com>
2022-12-14 12:51:56 +0100

    updated installer

---
+++ abides-jpmc-public/easy_install_unix.sh
@@ -0,0 +1,6 @@
+python -m venv lambda
+source lambda/bin/activate
+python -m pip install --upgrade pip
+python -m pip install -r requirements.txt
+pip install -e .
+
---
+++ abides-jpmc-public/easy_install_windows.sh
@@ -0,0 +1,7 @@
+python -m venv lambda
+.\\lambda\\Scripts\\activate
+python -m pip install --upgrade pip
+python -m pip install -r requirements.txt
+pip install -e .
+
+
